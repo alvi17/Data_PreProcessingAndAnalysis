37355f42c823ba5cc77bc4319e5e9cb7b370e6f5
sabid-habib <sabidbinhabib@gmail.com>
2015-11-03 02:07:26 +0600

    some message

--- src/mainpackage/PreprocessAttendance.java
+++ src/mainpackage/PreprocessAttendance.java
@@ -1,7 +1,10 @@
 package mainpackage;
 
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class PreprocessAttendance {
 	String fileName1="sheet1.csv";
@@ -12,22 +15,73 @@ public class PreprocessAttendance {
 	String fileName6="sheet6.csv";
 	String fileName7="sheet7.csv";
 	String fileName8="sheet8.csv";
+	String proFileName1="processed_sheet1.csv";
+	String proFileName2="processed_sheet2.csv";
+	String proFileName3="processed_sheet3.csv";
+	String proFileName4="processed_sheet4.csv";
+	String proFileName5="processed_sheet5.csv";
+	String proFileName6="processed_sheet6.csv";
+	String proFileName7="processed_sheet7.csv";
+	String proFileName8="processed_sheet8.csv";
+	FileWriter fileWriter = null;
 	public void readCsv()
 	{
 		
 		BufferedReader fileReader=null;
+		//BufferedReader fileReaderIn=null;
 		try{
 			String line="";
 			fileReader =new BufferedReader(new FileReader(fileName1));
-			fileReader.readLine();
-			//System.out.println("asd");
+			fileWriter = new FileWriter(proFileName1);
+			line=fileReader.readLine();
+			String[] parts=line.split(",");
+			
+			//System.out.println(parts.length);
+			for(int i=0;i<parts.length;i++){
+				
+				//System.out.println(parts[i]);
+				int count=0;
+				//fileReaderIn=null;
+				//fileReaderIn =new BufferedReader(new FileReader(fileName1));
+				
+				//fileReaderIn.readLine();
+				
+				
+					fileWriter.append(parts[i]);
+					if((parts.length-1)!=i){
+						fileWriter.append(",");
+					}
+				
+				
+				
+					
+				
+				
+			}
+				
+				
+			//int count=0;
+			
 			while((line=fileReader.readLine())!=null)
 			{
-				
+				fileWriter.append("\n");
 				String[] token=line.split(",");
-				if(token[11].equals(null)){
+				for(int i=0;i<token.length;i++){
+					if(parts[i].equals("CLASSATTENDENCEMARKS") && !token[i].equals("")){
+						//System.out.println(parts[i]);
+						fileWriter.append(token[i]);
+					}else if(parts[i].equals("CLASSATTENDENCEMARKS") && token[i].equals("")){
+						fileWriter.append("INF");
+					}else{
+						fileWriter.append(token[i]);
+					}
 					
+					if((token.length-1)!=i){
+						fileWriter.append(",");
+					}
 				}
+				
+				//System.out.println(count);
 			}
 		}catch(Exception c){
 			
