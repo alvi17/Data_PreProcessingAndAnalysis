01980733fc684f49f75c06a1ea38b259fef5a974
unknown <alvi1005017@gmail.com>
2015-11-23 23:12:22 +0600

    Adding some new csv

--- src/mainpackage/Data_PreProcessing_CreditColumn.java
+++ src/mainpackage/Data_PreProcessing_CreditColumn.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Data_PreProcessing_CreditColumn {
-	String fileName="processed_sheet2.csv";
+	String fileName="processed_sheet5.csv";
 	FileWriter fileWriter = null;
 	
 	public Data_PreProcessing_CreditColumn()
@@ -20,23 +20,25 @@ public class Data_PreProcessing_CreditColumn {
 	public void create_Credit_Csv()
 	{
 		BufferedReader fileReader = null;
-		
+		 int num=0;
 		List id=new ArrayList();
 		List attendance=new ArrayList();
 		List totalCredit=new ArrayList();
 		List completedCredit=new ArrayList();
 		List cgpaList=new ArrayList();
 		int sessionalCourse=0;
+		int serial_num=0;
+		int non_credit=0;
 		try{
 		String line = "";
 		fileReader = new BufferedReader(new FileReader(fileName));
-	//	fileWriter = new FileWriter("processed_sheet1_withCredit.csv");
+		fileWriter = new FileWriter("processed_sheet5_withCredit.csv");
 		String s=fileReader.readLine();
 		String[] parts=s.split(",");
-	//	fileWriter.append("SERIAL,DEPARTMENTID,HALLSTATUS,Gender,STARTINGACADEMICYEAR2,ADMISSIONDATE,ADMISSIONDATE,LETTERGRADE,CLASSATTENDENCEMARKS,CLASSTESTMARKS,PARTAMARKS,PARTBMARKS,TOTALNUMBER,LEVELNAME,TERMNAME,GPA,CGPA,TERMCOUNT,CRHREARNED,TOTCRHRCOMPLETED");
-	//	fileWriter.append("\n");
+		fileWriter.append("SERIAL,DEPARTMENTID,HALLSTATUS,Gender,STARTINGACADEMICYEAR2,ADMISSIONDATE,ADMISSIONDATE,LETTERGRADE,CLASSATTENDENCEMARKS,CLASSTESTMARKS,PARTAMARKS,PARTBMARKS,TOTALNUMBER,LEVELNAME,TERMNAME,GPA,CGPA,TERMCOUNT,CRHREARNED,TOTCRHRCOMPLETED,COURSECREDIT");
+		fileWriter.append("\n");
 		
-		//int i=1;
+		//	int i=1;
 		int serial=100;
 		int prev_serial=0;
 		String[] tokens=null;
@@ -45,100 +47,185 @@ public class Data_PreProcessing_CreditColumn {
 		 double totalNumber=0;
 		 int sessional=0;
 		 double credit=0;
-		while ((line = fileReader.readLine()) != null) {
+		 
+		while ((line = fileReader.readLine()) != null){
 			 String[] token = line.split(",");
 			 String c_complete,result_status,t_status;
 			 double a,t,c;
 			 int level=0;
 			 int term=0;
-			 
+			 serial_num++;
+			 fileWriter.append(line);
 			 for(int i=0;i<token.length;i++)
 			 {
-				 if(parts[i].equals("LETTERGRADE") && !token[i].equals("NR"))
+				 if(parts[i].equals("LETTERGRADE") )
 				 {
 					 grades = token[i];	 
 				 }
-				 else if(parts[i].equals("TOTALNUMBER") && (!token[i].equals("Abs") && !token[i].equals("NR")))
+				 else if(parts[i].equals("TOTALNUMBER"))
 				 {
-					 if(!token[i].equals("Cont") && !token[i].equals("Sat") && !token[i].equals("Unsat") &&!token[i].equals("Fail"))
-						 totalNumber=Double.parseDouble(token[i]);					 
+					 if(!token[i].equals("Abs") && !token[i].equals("NR") && !token[i].equals("Cont") && !token[i].equals("Sat") && !token[i].equals("Unsat") &&!token[i].equals("Fail"))
+					 {
+						 totalNumber=Double.parseDouble(token[i]);	
+						 if(token[i].endsWith("9") || token[i].endsWith("24")||token[i].endsWith("54")||token[i].endsWith("84")||token[i].endsWith("94"))
+						 {
+							 totalNumber=totalNumber+1;
+						 }
+					 }	
+					 else
+					 {
+						 totalNumber=0;
+					 }
 				 }
 				 else if(parts[i].equals("COURSENUMBER"))
 				 {
 					 courseName=token[i];
+					 if(courseName.length()>0)
+					 {
 					 sessional=Integer.parseInt(courseName.substring(courseName.length()-1))%2;
+					 }
 				 }				  		
 			 }
 			 int x=0;
+			 double percent=0;
 			 //check for sessional course
-			 if(sessional==0 && totalNumber<=150)
+			 if(sessional==0 && totalNumber==0)
 			 {
-				 double percent=totalNumber*100/150;
+				 non_credit++;
+				 if(grades.equals("S") || grades.equals("U") || grades.equals("X")||grades.equals("NR"))
+				 {
+					 credit=0;
+					
+					 x=1;
+				 }
+				 fileWriter.append(","+credit);
+				 
+			 }
+			 else if(sessional==0 && totalNumber<=150)
+			 {
+				 percent=totalNumber*100/150;
 				// System.out.println(percent)
-				 if((percent>=80 && grades.equals("A+"))||(percent<80 && percent>=75 && grades.equals("A"))||(percent<75 && percent>=70 && grades.equals("-A"))|| (percent<70 && percent>=65 && grades.equals("B+"))||(percent<65 && percent>=60 && grades.equals("B"))||(percent<60 && percent>=55 && grades.equals("B-"))||
+				 if((percent>=80 && grades.equals("A+"))||(percent<80 && percent>=75 && grades.equals("A"))||(percent<75 && percent>=70 && grades.equals("A-"))|| (percent<70 && percent>=65 && grades.equals("B+"))||(percent<65 && percent>=60 && grades.equals("B"))||(percent<60 && percent>=55 && grades.equals("B-"))||
 						 (percent<55 && percent>=50 && grades.equals("C+"))||(percent<50 && percent>=45 && grades.equals("C"))||(percent<45 && percent>=40 && grades.equals("D"))||(percent<40 && grades.equals("F")))
 				 {
 					 credit=1.5;
 					 x=1;
-					 //System.out.println(",1.5,");
+					 //	System.out.println(",1.5,");
 				 }
-				 else 
-				 {
-					 if(!grades.equals("S") || !grades.equals("U") || !grades.equals("X"))
-					 {
-					//	 credit=0.75;
-					 }
-				 }
+				 
 				 
 				 if(totalNumber<=75)
 				 {
 					 double percent1=totalNumber*100/75;
 						// System.out.println(percent)
-						 if((percent1>=80 && grades.equals("A+"))||(percent1<80 && percent1>=75 && grades.equals("A"))||(percent1<75 && percent1>=70 && grades.equals("-A"))|| (percent1<70 && percent1>=65 && grades.equals("B+"))||(percent1<65 && percent1>=60 && grades.equals("B"))||(percent1<60 && percent1>=55 && grades.equals("B-"))||
+						 if((percent1>=80 && grades.equals("A+"))||(percent1<80 && percent1>=75 && grades.equals("A"))||(percent1<75 && percent1>=70 && grades.equals("A-"))|| (percent1<70 && percent1>=65 && grades.equals("B+"))||(percent1<65 && percent1>=60 && grades.equals("B"))||(percent1<60 && percent1>=55 && grades.equals("B-"))||
 								 (percent1<55 && percent1>=50 && grades.equals("C+"))||(percent1<50 && percent1>=45 && grades.equals("C"))||(percent1<45 && percent1>=40 && grades.equals("D"))||(percent1<40 && grades.equals("F")))
 						 {
 							 credit=.75;
 							 x=1;
-							 //System.out.println(",1.5,");
+							 //	System.out.println(",1.5,");
 						 }
-						 else 
-						 {
-							 if(!grades.equals("S") || !grades.equals("U") || !grades.equals("X"))
-							 {
-								// credit=1.5;
-							 }
-						 }
-					 
+						 
 				 }
 				 if(x==0)
 				 {
 					 credit=1.5;
-				 }
-				 
+				 }				 
 				 sessionalCourse++;
+				 fileWriter.append(","+credit);
 				 System.out.println(courseName+","+"Percent :"+percent+","+credit+",");
 			 }
+			 else if(courseName.contains("400") &&  !grades.equals("X"))
+			 {
+				 //thesis
+				credit=6;
+				fileWriter.append(","+credit);
+				System.out.println(courseName+","+"Percent :"+percent+","+credit+",");
+ 
+			 }
 			 
-			 
-			 //fileWriter.append(token[0]+","+token[5]+","+token[19]+","+token[22]+","++"\n");	
-			 		 
-		}
-			 
-			
-			 			 
-//			 if(serial!=prev_serial)
-//			 {
-//				 prev_serial=serial;
-//				 if(tokens.length>0)
-//				 {
-//					  int attendance_mark=Integer.parseInt(tokens[1]);
-//					  float total_credit=Float.parseFloat(tokens[2]);
-//					  float term_count=Float.parseFloat(tokens[3]);
-//					  float credit_completed=Float.parseFloat(tokens[4]);
-//					  float cgpa=Float.parseFloat(tokens[5]);
-//				 }
-			 
-			
+			 else
+			 {
+				 num++;
+				 //theory courses
+				 double p1=totalNumber/200*100; //2 credit
+				 double p2=totalNumber/300*100; //3 credit
+				 double p3=totalNumber/400*100; //4 credit
+				 
+				 //check for 2 credit course
+				 
+				 if(totalNumber<=200)
+				 {
+					 if((p1>=80 && grades.equals("A+")) || (p1<80 && p1>=75 && grades.equals("A"))||(p1<75 && p1>=70 && grades.equals("A-")) 
+							 ||(p1<70 && p1>=65 && grades.equals("B+"))||(p1<65 && p1>=60 && grades.equals("B")) || (p1<60 && p1>=55 && grades.equals("B-"))||(p1<55 && p1>=50 && grades.equals("C+"))
+							 ||(p1<50 && p1>=45 && grades.equals("C")) ||(p1<45 && p1>=40 && grades.equals("D")))
+					 {
+						 credit = 2;
+					 }
+					 
+					 if((p2>=65 && grades.equals("B+")) || (p2<65 && p2>=60 && grades.equals("B")) || (p2<60 && p2>=55 && grades.equals("B-"))
+							 ||(p2<55 && p2>=50 && grades.equals("C+")) || (p2<50 && p2>=45 && grades.equals("C")) || (p2<45 && p2>=40 && grades.equals("D")))
+					 {
+						 credit=3;
+					 }
+					 
+					 if((p3<=50 && p3>=45 && grades.equals("C")) || (p3<=45 && p3>=40 && grades.equals("D")))
+					 {
+						 credit=4;
+					 }
+					 if(p3<40 && grades.equals("F"))
+					 {
+						 credit=4;
+					 }
+					 if(p2<40 && grades.equals("F"))
+					 {
+						 credit=3;
+					 }
+					 if(p1<40 && grades.equals("F"))
+					 {
+						 credit=2;
+					 }
+					 
+					 System.out.println(courseName+","+"Percent :"+percent+","+credit+",");
+				 }
+				 else if(totalNumber<=300 && totalNumber>200)
+				 {
+					 //check for 3 credit
+					 if((p2>=80 && grades.equals("A+")) || (p2<80 && p2>=75 && grades.equals("A"))
+							 ||(p2<75 && p2>=70 && grades.equals("A-")) 
+							 ||(p2<70 && p1>=65 && grades.equals("B+"))){
+						 credit = 3;
+					 }
+					 //check for 4 credit
+					 if((p3<75 && p3>=70 && grades.equals("A-"))||(p3<70 && p3>=65 && grades.equals("B+"))||(p3<65 && p3>=60 && grades.equals("B"))
+							 ||(p3<60 && p3>=55 && grades.equals("B-"))||(p3<55 && p3>=50 && grades.equals("C+")))
+					 {
+						 credit = 4;
+					 }
+					 System.out.println(courseName+","+"Percent :"+percent+","+credit+",");
+				 }
+				 
+				 else if(totalNumber>300)
+				 {
+					 //check for 3 credit
+					 
+					// if((p3>=80 && grades.equals("A+"))||(p3<80 && p3>=75 && grades.equals("A")))
+				//	 {
+						 credit = 4;
+					 //}
+					 System.out.println(courseName+","+"Percent :"+percent+","+credit+",");
+					 //check for 4 credit
+				 }
+				 else
+				 {
+					 credit=3;
+					 System.out.println(courseName+","+"Percent :"+percent+","+credit+",");
+				 }
+				 fileWriter.append(","+credit);
+			 }
+			fileWriter.append("\n");
+			credit=0;
+		}			
 		}
 		catch(Exception e)
 		{
@@ -149,14 +236,14 @@ public class Data_PreProcessing_CreditColumn {
 			try
 			{
 				fileReader.close();
-			//	fileWriter.flush();
-			//	fileWriter.close();
+				fileWriter.flush();
+				fileWriter.close();
 			}
 			catch(IOException e)
 			{
 				e.printStackTrace();
 			}
 		}
-		System.out.println("Total Sessional Course :"+sessionalCourse);
+		System.out.println("Total Sessional Course :"+sessionalCourse +"Total theory course: "+num +" Total course: "+serial_num +" Sum: "+(sessionalCourse+num+non_credit));
 	}
 }
